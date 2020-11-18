14d98cb875b521338430d442e6b9e93385adfa42
QOA <58850207+QAKANDE@users.noreply.github.com>
2020-11-18 18:54:44 +0000

    port chnaged

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const listEndpoints = require("express-list-endpoints");
 
 const server = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3003;
 server.use(cors());
 server.use(express.json());
 server.use("/users", users);
